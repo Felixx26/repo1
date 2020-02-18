3d8bdda413aed0e4bec8cd07e4813973c07fbd93
TitoMatos <titomatos-0@hotmail.com>
2020-02-18 16:38:34 -0400

    Variables de prueba.

--- app/src/main/java/com/example/handsup/MainActivity.java
+++ app/src/main/java/com/example/handsup/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        int i,u,q;
+        i = 10;
+        u = 10;
+        q = i + u;
+
+
+
     }
 }
